f486b8de1bdff5e55ec3a9a3ff7ac8fdead435c8
Yury Silushkin <yusilushkin@yandex.ru>
2020-02-02 19:51:06 +0300

    Added greetings function and prompt module

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,9 @@
-def main():
-    print("Welcome to the Brain Games!")
+import brain_games.cli
 
+def main():
+    print('Welcome to the Brain Games!')
+    print()
+    brain_games.cli.welcome_user()
 
 
 if __name__ == '__main__':
