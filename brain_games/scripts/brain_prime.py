b39146ab65f9b369799c58780e3bc38bc8666b98
Yury Silushkin <yusilushkin@yandex.ru>
2020-03-19 21:12:33 +0300

    Remove needless commentary

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python3
 
-# import brain_games.cli
-# import brain_games.main_loop
-
 import brain_games.games.prime
 
 
 def main():
     brain_games.games.prime.start_prime_game()
-
-    # brain_games.cli.welcome_gcd_rules()
-    # user_name = brain_games.cli.welcome_user()
-    # brain_games.main_loop.main_loop('prime', user_name)
 
 
 if __name__ == '__main__':
