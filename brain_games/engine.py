e7e0f773b251fa31f308e2420878c61efbf3a101
Yury Silushkin <yusilushkin@yandex.ru>
2020-04-01 19:38:52 +0300

    Changes the IF statement
    Removing unnecessary else-branch. Issue #29.

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -17,11 +17,12 @@ def play(get_game_question_and_answer=None, description=''):
         print('Question:', question)
         user_answer = prompt.string('Your answer: ')
 
-        if user_answer == right_answer:
-            print('Correct!\n')
-            attempts_count -= 1
-        else:
+        if user_answer != right_answer:
             print(f'\"{user_answer}\" is wrong answer ;(. '
                   f'Correct answer was \"{right_answer}\". '
                   f'Let\'s try again, {user_name}!\n')
+            continue
+        print('Correct!\n')
+        attempts_count -= 1
+
     print(f'Congratulations, {user_name}!')
