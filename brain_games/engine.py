e6c728ca7f46a368fbe3bec27ac7da6c07454fa9
Yury Silushkin <yusilushkin@yandex.ru>
2020-04-02 10:22:57 +0300

    Removes training script and related code.
    Issue #28

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,16 +1,13 @@
 import prompt
 
 
-def play(get_game_question_and_answer=None, description=''):
+def play(get_game_question_and_answer, description):
     attempts_count = 3
 
     print('Welcome to Brain Games!')
     print(f'{description}\n')
     user_name = prompt.string('May I have your name? ')
     print(f'Hello, {user_name}!\n')
-
-    if not description:
-        quit()
 
     while attempts_count:
         question, right_answer = get_game_question_and_answer()
