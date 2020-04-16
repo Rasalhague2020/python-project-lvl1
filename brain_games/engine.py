b5d4da10a33ee26334782002a1d30166caa60c0e
Yury Silushkin <yusilushkin@yandex.ru>
2020-04-16 08:50:51 +0300

    Use local variable instead of global as loop counter

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -5,14 +5,14 @@ ATTEMPTS_COUNT = 3
 
 
 def play(get_game_question_and_answer, description):
-    global ATTEMPTS_COUNT
+    current_attempts_count = ATTEMPTS_COUNT
 
     print('Welcome to Brain Games!')
     print(f'{description}\n')
     user_name = prompt.string('May I have your name? ')
     print(f'Hello, {user_name}!\n')
 
-    while ATTEMPTS_COUNT:
+    while current_attempts_count:
         question, right_answer = get_game_question_and_answer()
         print('Question:', question)
         user_answer = prompt.string('Your answer: ')
@@ -23,6 +23,6 @@ def play(get_game_question_and_answer, description):
                   f'Let\'s try again, {user_name}!\n')
             continue
         print('Correct!\n')
-        ATTEMPTS_COUNT -= 1
+        current_attempts_count -= 1
 
     print(f'Congratulations, {user_name}!')
