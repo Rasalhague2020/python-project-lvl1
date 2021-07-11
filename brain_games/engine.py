305e0af4a99d21c455789d3954235143f4b2320d
Yury Silushkin <yusilushkin@yandex.ru>
2021-07-11 15:56:25 +0300

    Small exit fix. Issue #3.

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -21,7 +21,7 @@ def play(get_game_question_and_answer, description):
             print(f'\"{user_answer}\" is wrong answer ;(. '
                   f'Correct answer was \"{right_answer}\". '
                   f'Let\'s try again, {user_name}!\n')
-            exit()
+            return
         print('Correct!\n')
 
     print(f'Congratulations, {user_name}!')
