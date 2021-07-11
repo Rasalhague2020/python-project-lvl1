5e03a45dc7f164e71149f7c78c55361ddc14aae7
Yury Silushkin <yusilushkin@yandex.ru>
2021-07-11 12:38:10 +0300

    Hide comparison into gcd function. Issue #7.

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -13,11 +13,13 @@ def get_gcd_question_and_answer():
     num2 = random.randint(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
 
     question = f'{num1} {num2}'
-    answer = str(get_gcd(max(num1, num2), min(num1, num2)))
+    answer = str(get_gcd(num1, num2))
     return question, answer
 
 
 def get_gcd(a, b):
+    if b > a:
+        a, b = b, a
     if a % b == 0:
         return b
     return get_gcd(b, a % b)
